0bd02c73ae12e01859087036e172b45034c8b7ac
Leonardo Perdomo <leonardop763@gmail.com>
2024-03-29 15:37:33 -0400

    Refactor: Overview design changed. Planner and Compliance Chart added.

---
+++ src/sections/overview/app-planner.jsx
@@ -0,0 +1,150 @@
+import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
+import '@bitnoi.se/react-scheduler/dist/style.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { Scheduler } from '@bitnoi.se/react-scheduler';
+
+import { Card, Stack, Button, Container, CardHeader } from '@mui/material';
+
+import { openDialog } from 'src/features/dialogs/dialogsSlice';
+// import {
+//   fetchEmployees,
+//   selectEmployees,
+//   selectEmployeeStatus,
+// } from 'src/features/employee/employeeSlice';
+import {
+  fetchEquipment,
+  selectEquipment,
+  selectEquipmentStatus,
+} from 'src/features/equipment/equipmentSlice';
+import {
+  fetchMaintenance,
+  selectMaintenance,
+  selectMaintenanceStatus,
+} from 'src/features/maintenance/maintenanceSlice';
+import {
+  fetchMaintenanceTasks,
+  selectMaintenanceTasks,
+  selectMaintenanceTaskStatus,
+} from 'src/features/maintenance/maintenanceTaskSlice';
+
+import Iconify from 'src/components/iconify';
+
+export default function AppPlanner({ title, subheader }) {
+  const dispatch = useDispatch();
+  const maintenanceTasks = useSelector(selectMaintenanceTasks);
+  const maintenanceTaskStatus = useSelector(selectMaintenanceTaskStatus);
+  const equipment = useSelector(selectEquipment);
+  const equipmentStatus = useSelector(selectEquipmentStatus);
+  //   const employees = useSelector(selectEmployees);
+  //   const employeeStatus = useSelector(selectEmployeeStatus);
+  const maintenances = useSelector(selectMaintenance);
+  const maintenanceStatus = useSelector(selectMaintenanceStatus);
+
+  const [filterButtonState, setFilterButtonState] = useState(0);
+
+  useEffect(() => {
+    if (maintenanceTaskStatus === 'idle') {
+      dispatch(fetchMaintenanceTasks());
+    }
+  }, [maintenanceTaskStatus, dispatch]);
+
+  useEffect(() => {
+    if (equipmentStatus === 'idle') {
+      dispatch(fetchEquipment());
+    }
+  }, [equipmentStatus, dispatch]);
+
+  //   useEffect(() => {
+  //     if (employeeStatus === 'idle') {
+  //       dispatch(fetchEmployees());
+  //     }
+  //   }, [employeeStatus, dispatch]);
+
+  useEffect(() => {
+    if (maintenanceStatus === 'idle') {
+      dispatch(fetchMaintenance());
+    }
+  }, [maintenanceStatus, dispatch]);
+
+  const getColorByStatus = (status) => {
+    const statusColors = {
+      Pendiente: 'red',
+      'En Progreso': 'yellow',
+      Ejecutado: 'green',
+      Cancelado: 'gray',
+      Reprogramado: 'orange',
+    };
+
+    return statusColors[status] || 'blue';
+  };
+
+  const events = maintenances.map((maintenance) => {
+    const tasks = maintenanceTasks.filter((task) => task.maintenanceId === maintenance.id);
+    const equipmentInfo = equipment.find((eq) => eq.id === maintenance.equipmentId);
+
+    return {
+      id: maintenance.id,
+      label: {
+        icon: equipmentInfo?.photo || 'https://picsum.photos/24',
+        title: equipmentInfo?.name || 'Equipo Desconocido',
+        subtitle: maintenance.description,
+      },
+      data: tasks.map((task) => {
+        const color = getColorByStatus(task.status);
+
+        return {
+          id: task.id,
+          startDate: new Date(task.startDate),
+          endDate: new Date(task.endDate),
+          occupancy: 2,
+          title: task.title,
+          subtitle: task.status,
+          bgColor: color,
+        };
+      }),
+    };
+  });
+
+  const handleTileClick = (clickedResource) => {
+    dispatch(openDialog({ dialogType: 'showMaintenanceTask', data: clickedResource.id }));
+  };
+
+  const handleFilter = () => {
+    setFilterButtonState(filterButtonState + 1);
+  };
+
+  return (
+    <Card>
+      <CardHeader title={title} subheader={subheader} sx={{ mb: 5 }} />
+      <div style={{ height: 380, padding: 20 }}>
+        <Scheduler
+          data={events}
+          //   isLoading={isLoading}
+          onRangeChange={(newRange) => console.log(newRange)}
+          onTileClick={handleTileClick}
+          onItemClick={(item) => console.log(item)}
+          onFilterData={() => {
+            // Some filtering logic...
+            setFilterButtonState(1);
+          }}
+          onClearFilterData={() => {
+            // Some clearing filters logic...
+            setFilterButtonState(0);
+          }}
+          config={{
+            zoom: 1,
+            filterButtonState,
+            includeTakenHoursOnWeekendsInDayView: true,
+          }}
+          lang="es"
+        />
+      </div>
+    </Card>
+  );
+}
+
+AppPlanner.propTypes = {
+  title: PropTypes.string,
+  subheader: PropTypes.string,
+};
--- src/sections/overview/view/app-view.jsx
+++ src/sections/overview/view/app-view.jsx
@@ -1,6 +1,6 @@
+import axios from 'axios';
 import { faker } from '@faker-js/faker';
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
@@ -8,7 +8,10 @@ import Typography from '@mui/material/Typography';
 
 import Iconify from 'src/components/iconify';
 
+import ShowMaintenanceTaskDialog from 'src/sections/maintenance/crud/show-maintenance-form-dialog';
+
 import AppTasks from '../app-tasks';
+import AppPlanner from '../app-planner';
 import AppNewsUpdate from '../app-news-update';
 import AppOrderTimeline from '../app-order-timeline';
 import AppCurrentVisits from '../app-current-visits';
@@ -17,6 +20,7 @@ import AppWidgetSummary from '../app-widget-summary';
 import AppTrafficBySite from '../app-traffic-by-site';
 import AppCurrentSubject from '../app-current-subject';
 import AppConversionRates from '../app-conversion-rates';
+import AppMaintenanceCompliance from '../app-maintenance-compliance';
 
 // ----------------------------------------------------------------------
 
@@ -26,7 +30,9 @@ export default function AppView() {
   useEffect(() => {
     const fetchMonthlyCompletionRates = async () => {
       try {
-        const response = await axios.get('http://localhost:5263/api/maintenancechart/monthly-completion');
+        const response = await axios.get(
+          'http://localhost:5263/api/maintenancechart/monthly-completion'
+        );
         setMonthlyCompletionRates(response.data);
       } catch (error) {
         console.error('Error fetching monthly completion rates:', error);
@@ -40,18 +46,17 @@ export default function AppView() {
   const series = Object.values(monthlyCompletionRates).map((value) => (value * 100).toFixed(1));
   const average = series.reduce((acc, value) => acc + parseFloat(value), 0) / series.length;
 
-
   return (
     <Container maxWidth="xl">
       <Typography variant="h4" sx={{ mb: 5 }}>
-        Hi, Welcome back 👋
+        Hola, bienvenido 👋
       </Typography>
 
       <Grid container spacing={3}>
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
-            title="Weekly Sales"
-            total={714000}
+            title="Mantenimientos Completados"
+            total={1}
             color="success"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
           />
@@ -59,7 +64,7 @@ export default function AppView() {
 
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
-            title="New Users"
+            title="$ Total Gastado"
             total={1352831}
             color="info"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />}
@@ -68,8 +73,8 @@ export default function AppView() {
 
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
-            title="Item Orders"
-            total={1723315}
+            title="Equipos Comprados"
+            total={2}
             color="warning"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
           />
@@ -77,15 +82,19 @@ export default function AppView() {
 
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
-            title="Bug Reports"
-            total={234}
+            title="Mantenimientos el próximo mes"
+            total={4}
             color="error"
             icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
           />
         </Grid>
 
         <Grid xs={12} md={6} lg={8}>
-          <AppWebsiteVisits
+          <AppPlanner title="Planificador de mantenimiento" subheader="(+43%) than last year" />
+        </Grid>
+
+        <Grid xs={12} md={6} lg={4}>
+          <AppMaintenanceCompliance
             title="Cumplimiento de mantenimiento"
             subheader={`Year average: ${average.toFixed(1)}%`}
             chart={{
@@ -101,21 +110,16 @@ export default function AppView() {
                   name: 'Esperado',
                   type: 'area',
                   fill: 'gradient',
-                  data: [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100],
+                  data: series.map(() => 96),
                 },
-                // {
-                //   name: 'Team C',
-                //   type: 'line',
-                //   fill: 'solid',
-                //   data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
-                // },
               ],
               colors: ['#F67A5B'],
             }}
           />
         </Grid>
 
-        <Grid xs={12} md={6} lg={8}>
+        <ShowMaintenanceTaskDialog />
+        {/* <Grid xs={12} md={6} lg={8}>
           <AppWebsiteVisits
             title="Website Visits"
             subheader="(+43%) than last year"
@@ -276,7 +280,7 @@ export default function AppView() {
               { id: '5', name: 'Sprint Showcase' },
             ]}
           />
-        </Grid>
+        </Grid> */}
       </Grid>
     </Container>
   );
