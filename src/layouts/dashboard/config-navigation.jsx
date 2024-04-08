e7769bca0771cbde68bc8a495ab7200d5363698d
Leonardo Perdomo <leonardop763@gmail.com>
2024-04-08 02:17:55 -0400

    Add: @mui/x-date-pickers.

--- src/layouts/dashboard/config-navigation.jsx
+++ src/layouts/dashboard/config-navigation.jsx
@@ -22,11 +22,11 @@ const navConfig = [
     path: '/maintenance',
     icon: icon('ic_maintenance'),
   },
-  {
-    title: 'planificador',
-    path: '/schedule',
-    icon: icon('ic_planner'),
-  },
+  // {
+  //   title: 'planificador',
+  //   path: '/schedule',
+  //   icon: icon('ic_planner'),
+  // },
   // {
   //   title: 'user',
   //   path: '/user',
